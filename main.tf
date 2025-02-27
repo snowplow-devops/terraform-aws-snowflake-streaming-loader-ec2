e9791d56375dcad964bebd6c37e025656af08fff
jbeemster <joshua.a.beemster@gmail.com>
2025-02-27 12:03:49 +0100

    Prepared for release

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   module_name    = "snowflake-streaming-loader-ec2"
-  module_version = "0.1.2"
+  module_version = "0.1.3"
 
   app_name    = "snowflake-loader"
   app_version = var.app_version
